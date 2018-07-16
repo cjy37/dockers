9f36722a9962508c6382e047bddf3115fb40c1bc
陈景远 <rocky.cn@foxmail.com>
2018-07-16 11:57:52 +0800

    nginx

--- nginx/default.conf.sh
+++ nginx/default.conf.sh
@@ -15,18 +15,15 @@ server {
     listen ${ADMIN_PORT} default_server;
     listen [::]:${ADMIN_PORT} default_server;
 
-    server_name _ ${ADMIN_DOMAIN};
+    server_name ${ADMIN_DOMAIN};
 
     #root /wwwroot/htdocs/admin;
     #index index.html index.htm;
 
     location / {
-        # proxy_next_upstream http_500 http_502 http_503 http_504 error timeout invalid_header;
-        # proxy_cache cache_one;
-        # proxy_cache_valid any 10s;
-        # proxy_cache_valid 200 304 7d;
-        # proxy_cache_valid 301 3d;
-        # proxy_cache_key \$host\$uri\$is_args\$args;
+
+        proxy_pass http://admin;
+        
         proxy_redirect off;
         proxy_set_header X-Real-IP \$remote_addr;
         proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
@@ -39,25 +36,18 @@ server {
         #sendfile       on;
         tcp_nopush     on;
         aio            on;
-
-        proxy_pass http://admin;
     }
 
     location /joincas {
+        proxy_pass http://cas;
         proxy_redirect off;
         proxy_set_header X-Real-IP \$remote_addr;
         proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
         proxy_set_header Host \$host;
-
-        # proxy_http_version 1.1;
-        # proxy_set_header Upgrade \$http_upgrade;
-        # proxy_set_header Connection "upgrade";
-
+        
         #sendfile       on;
         tcp_nopush     on;
         aio            on;
-
-        proxy_pass http://cas;
     }
 }
 
@@ -65,18 +55,10 @@ server {
     listen ${API_PORT};
     listen [::]:${API_PORT};
 
-    server_name server_name ${API_DOMAIN};
-
-    #root /wwwroot/htdocs/admin;
-    #index index.html index.htm;
+    server_name ${API_DOMAIN};
 
     location / {
-        # proxy_next_upstream http_500 http_502 http_503 http_504 error timeout invalid_header;
-        # proxy_cache cache_one;
-        # proxy_cache_valid any 10s;
-        # proxy_cache_valid 200 304 7d;
-        # proxy_cache_valid 301 3d;
-        # proxy_cache_key \$host\$uri\$is_args\$args;
+        proxy_pass http://api;
         proxy_redirect off;
         proxy_set_header X-Real-IP \$remote_addr;
         proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
@@ -89,8 +71,6 @@ server {
         #sendfile       on;
         tcp_nopush     on;
         aio            on;
-
-        proxy_pass http://api;
     }
 }
 EOF
